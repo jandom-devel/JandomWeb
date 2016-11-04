4f9037496f51ba94f0005def2614631bb2430595
Gianluca Amato <gianluca.amato@unich.it>
2016-11-04 12:13:07 +0100

    Overhaul uploading of jar files for bytecode analysis. * JandomWeb does not decompress Jars anymore * Harcoded paths have been removed

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,16 +1,17 @@
 package controllers
 
+import java.nio.file._
+import javax.inject.Inject
+
 import it.unich.jandom._
 import models._
-import play.api.Play._
+import play.api.Environment
 import play.api.data.Form
 import play.api.data.Forms._
 import play.api.i18n.MessagesApi
 import play.api.mvc._
-import java.io.File
-import javax.inject._
 
-class Application @Inject() (implicit val messages: MessagesApi)  extends Controller  {
+class Application @Inject() (implicit val messages: MessagesApi, env: Environment)  extends Controller  {
 
   val menuItemCONST = Map[String, Int] ("INDEX"       -> 0,
                                         "CREDITS"     -> 1,
@@ -111,20 +112,20 @@ class Application @Inject() (implicit val messages: MessagesApi)  extends Contro
     )(ParameterBytecodeData.apply)(ParameterBytecodeData.unapply)
   }
 
-  val exampleDir = "/home/simone/workspace/activator-dist-1.3.6/JandomUI/public/example/java"
-  val jarDir = "/home/simone/workspace/activator-dist-1.3.6/JandomUI/public/tmpUpdateFile" + "/" + "jar"
-  var tmpDir = jarDir + "/"
+  val exampleDir = env.rootPath.toPath.resolve("public").resolve("example").resolve("java")
+  var jarFile = exampleDir
   var filename = ""
   var classSoot = Seq[String]()
   var methodsSoot = Seq[Seq[String]]()
   val irType = Seq ("Baf", "Jimple")
   val analysisType = Seq("Numerical", "Object")
 
-  def getKlassAndMethods (classPathField: String) : (Seq[String], Seq[Seq[String]]) = {
+  def getKlassAndMethods (dir: Path) : (Seq[String], Seq[Seq[String]]) = {
     var methods = new scala.collection.mutable.ListBuffer[Seq[String]]()
-    val classes = ui.OutputInterface.getClasses(classPathField)
+    val scene = soot.Scene.v()
+    val classes = ui.OutputInterface.getClasses(dir)
     for (klass <- (classes.zipWithIndex)){
-      methods += (ui.OutputInterface.getMethods(classPathField, klass._2))
+      methods += (ui.OutputInterface.getMethods(dir, klass._2))
     }
     (classes, methods.toSeq)
   }
@@ -135,16 +136,13 @@ class Application @Inject() (implicit val messages: MessagesApi)  extends Contro
   }
 
   def updateFileSoot = Action(parse.multipartFormData) { request =>
-    request.body.file("fileJar").map { fileJar =>
-      filename = fileJar.filename
-      fileJar.ref.moveTo(new File(jarDir + "/" + filename))
-      tmpDir = jarDir + "/" + filename.substring(0, filename.lastIndexOf('.'))
-      val tmpDirF = new File(tmpDir)
-      if (!tmpDirF.exists())
-        tmpDirF.mkdir()
-
-      ui.OutputInterface.unzipJar(jarDir, filename, tmpDir)
-      val (classSoot1, methodsSoot1) = getKlassAndMethods(tmpDir)
+    val uploadedJar = request.body.file("fileJar") filter ( _.ref.file.length() != 0 )
+    // Play 2.5.x has a bug which requires a convoluted approach for checking
+    // if a file has been uploaded: https://github.com/playframework/playframework/issues/6203
+    uploadedJar.map { uploadedJar =>
+      filename = uploadedJar.filename
+      jarFile = uploadedJar.ref.file.toPath
+      val (classSoot1, methodsSoot1) = getKlassAndMethods(jarFile)
       classSoot = classSoot1
       methodsSoot = methodsSoot1
 
@@ -154,12 +152,11 @@ class Application @Inject() (implicit val messages: MessagesApi)  extends Contro
         classSoot.zipWithIndex, methodsSeq, irType.zipWithIndex, analysisType.zipWithIndex)(menuItemCONST("ANALYZE_BC")))
 
     }.getOrElse {
-      val filename = request.body.asFormUrlEncoded.get("isExample").head(0)
+      filename = request.body.asFormUrlEncoded.get("isExample").head(0)
       if (filename != "-1"){
         Ok(views.html.home(filename, "Description", "", menuItemCONST("INDEX")))
-        tmpDir = exampleDir + "/" + filename.toString().substring(0, filename.toString().lastIndexOf('.'))
-//        tmpDir = filename.toString().substring(0, filename.toString().lastIndexOf('.'))
-        val (classSoot1, methodsSoot1) = getKlassAndMethods(tmpDir)
+        jarFile = exampleDir.resolve(filename)
+        val (classSoot1, methodsSoot1) = getKlassAndMethods(jarFile)
         classSoot = classSoot1
         methodsSoot = methodsSoot1
         val methodsSeq = methodsSoot.map(a => a.zipWithIndex).zipWithIndex
@@ -183,7 +180,7 @@ class Application @Inject() (implicit val messages: MessagesApi)  extends Contro
         val nB = models.ParameterBytecodeData (bcData.classes, bcData.method, bcData.irType, bcData.analysisType, bcData.parData)
         val isBaf = if (nB.irType == 0) true else false
         val methodsSeq = methodsSoot.map(a => a.zipWithIndex).zipWithIndex
-        val textCode = ui.OutputInterface.getSootAbstraction(tmpDir ,nB.classes.toInt, nB.method.toInt, isBaf)
+        val textCode = ui.OutputInterface.getSootAbstraction(jarFile, nB.classes.toInt, nB.method.toInt, isBaf)
         Ok(views.html.showFormBytecodeCode(filename, parametersForm, domain, objDom, widSco, narStr, parametersBytecode,
           classSoot.zipWithIndex, methodsSeq, irType.zipWithIndex, analysisType.zipWithIndex, textCode, nB)(menuItemCONST("ANALYZE_BC")))
       }
@@ -205,11 +202,11 @@ class Application @Inject() (implicit val messages: MessagesApi)  extends Contro
         val isNum = if (nB.analysisType == 0) true else false
         val debug = false
         val methodsSeq = methodsSoot.map(a => a.zipWithIndex).zipWithIndex
-        val textCode = ui.OutputInterface.getSootAbstraction(tmpDir, nB.classes.toInt, nB.method.toInt, isBaf)
+        val textCode = ui.OutputInterface.getSootAbstraction(jarFile, nB.classes.toInt, nB.method.toInt, isBaf)
 
         val origine = request.body.asFormUrlEncoded.get("origine")
         if (origine.head == "0"){
-          val res = ui.OutputInterface.analyze(tmpDir, klass, method, isNum, isBaf, nB.parData.domain,
+          val res = ui.OutputInterface.analyze(jarFile, klass, method, isNum, isBaf, nB.parData.domain,
             nB.parData.widScope, nB.parData.narrStrategy, nB.parData.delay, debug)
 
           Ok(views.html.resultBytecode(nB.parData.code, res)(menuItemCONST("ANALYZE_BC")))
